b2bc79a31fa01bb925a3f35e01add5eef7007aa7
elly <elly.chan.mda@gmail.com>
2019-01-15 15:49:29 +0000

    add .placeSpot with test

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -6,8 +6,12 @@ describe("Player", function() {
     game = new Game();
   });
 
-  it ('return a new array of nine as the game borad', function() {
+  it ('#.board return a new array of nine as the game borad', function() {
     expect(game.board).toEqual([undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined]);
+  })
+
+  it ('#.placeSpot change the borad element by index number', function() {
+    expect(game.placeSpot(3)).toEqual([undefined, undefined, undefined, "O", undefined, undefined, undefined, undefined, undefined]);
   })
 
 });
--- src/Game.js
+++ src/Game.js
@@ -4,6 +4,9 @@ function Game() {
   this.board = new Array(9);
 }
 
-
+Game.prototype.placeSpot = function(indexNumber) {
+  this.board[indexNumber] = "O"
+  return this.board
+};
 
 module.exports = Game;
